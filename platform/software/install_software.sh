c05fa5def89cab120b3369c5e9169e7c9c2be01e
Leeonky <leeonky@gmail.com>
2016-02-12 11:40:04 +0800

    disable root remote login

--- platform/software/install_software.sh
+++ platform/software/install_software.sh
@@ -1,6 +1,6 @@
 disable_root_remote_login() {
 	if cat "$1" | grep -q "^PermitRootLogin"; then
-		sed 's/^PermitRootLogin/PermitRootLogin no/g' -i $1
+		sed 's/^PermitRootLogin.*/PermitRootLogin no/g' -i $1
 	else
 		echo 'PermitRootLogin no' >> $1
 	fi
--- platform/software/install_software_test.sh
+++ platform/software/install_software_test.sh
@@ -18,6 +18,18 @@ EOF
 	disable_root_remote_login /tmp/sshconf
 
 	assertEquals '1' "$(cat /tmp/sshconf | grep ^PermitRootLogin | wc -l)"
+	assertEquals 'PermitRootLogin no' "$(cat /tmp/sshconf | grep ^PermitRootLogin )"
+}
+
+test_disable_root_remote_login_again(){
+	cat > /tmp/sshconf<<EOF
+#dummy
+PermitRootLogin no
+EOF
+	disable_root_remote_login /tmp/sshconf
+
+	assertEquals '1' "$(cat /tmp/sshconf | grep ^PermitRootLogin | wc -l)"
+	assertEquals 'PermitRootLogin no' "$(cat /tmp/sshconf | grep ^PermitRootLogin )"
 }
 
 
