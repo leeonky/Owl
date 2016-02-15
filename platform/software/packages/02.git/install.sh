f1430092c88ed200d83fdd1f66b82a3dbce44778
Leeonky <leeonky@gmail.com>
2016-02-15 17:50:51 +0800

    install git package

---
+++ platform/software/packages/02.git/install.sh
@@ -0,0 +1,2 @@
+local archives="perl-Error-0.17020-2.el7.noarch.rpm perl-TermReadKey-2.30-20.el7.x86_64.rpm git-1.8.3.1-6.el7.x86_64.rpm perl-Git-1.8.3.1-6.el7.noarch.rpm"
+local install="rpm -ivh --force"
