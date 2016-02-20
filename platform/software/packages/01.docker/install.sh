ba1e33f3b90e542e8f500b5a1d1d9056e45a9edd
Leeonky <leeonky@gmail.com>
2016-02-20 12:28:15 +0800

    minor fix

--- platform/software/packages/01.docker/install.sh
+++ platform/software/packages/01.docker/install.sh
@@ -1,12 +1,7 @@
 start_docker() {
-	if cat /etc/default/docker | grep -q 'daocloud.io'; then
-		echo "Already config daocloud.io."
-	else
-		echo "DOCKER_OPTS=\"\$DOCKER_OPTS --registry-mirror=http://bd5fd403.m.daocloud.io\"" | sudo tee -a /etc/default/docker
-	fi
-
 	echo 'ALL    ALL=(ALL)       NOPASSWD: /usr/bin/docker' > /etc/sudoers.d/docker
 
+	mkdir -p /root/.docker/
 	cat > /root/.docker/config.json <<EOF
 {
         "auths": {
@@ -17,6 +12,12 @@ start_docker() {
         }
 }
 EOF
+
+	if cat /etc/default/docker 2>/dev/null | grep -q 'daocloud.io'; then
+		echo "Already config daocloud.io."
+	else
+		echo "DOCKER_OPTS=\"\$DOCKER_OPTS --registry-mirror=http://bd5fd403.m.daocloud.io\"" | sudo tee -a /etc/default/docker
+	fi
 
 	systemctl enable docker.service
 	systemctl start docker.service
