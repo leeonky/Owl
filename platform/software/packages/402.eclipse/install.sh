140cc2d9d32f1efe0d8704e9c2865d0f5831eeae
Leeonky <leeonky@gmail.com>
2016-02-21 12:49:34 +0800

    local DISPLAY

--- platform/software/packages/402.eclipse/install.sh
+++ platform/software/packages/402.eclipse/install.sh
@@ -4,7 +4,7 @@ install_eclipse() {
 APP_VERSION=2.1.1
 docker_tools.sh retain daocloud.io/leeonky/eclipse-jdt-cdt \$APP_VERSION
 mkdir -p ~/eclipse_workspace
-sudo docker run --rm --privileged=true -e DISPLAY=\$DISPLAY -v ~:/home/devuser/volumn:rw daocloud.io/leeonky/eclipse-jdt-cdt:\$APP_VERSION eclipse -data /home/devuser/volumn/eclipse_workspace/ 2>&1 1>/dev/null
+sudo docker run --rm --privileged=true -e DISPLAY=\$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~:/home/devuser/volumn:rw daocloud.io/leeonky/eclipse-jdt-cdt:\$APP_VERSION eclipse -data /home/devuser/volumn/eclipse_workspace/ 2>&1 1>/dev/null
 EOF
 	chmod a+x "$eclipse_bin"
 }
